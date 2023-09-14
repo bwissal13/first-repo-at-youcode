1bb6759459c678dd6e32288a439fb946100581df
bwissal <baazizwissal13@gmail.com>
2023-09-14 18:31:14 +0100

    add challenge 2 in condition

---
+++ lesConditions/challenge2/challenge2.c
@@ -0,0 +1,49 @@
+
+
+int main()
+{
+  char caractere;
+    printf("Entrer le caractere:");
+    scanf("%c",&caractere);
+      switch(caractere) {
+              case 'a':
+              printf("c'est une voyelle");
+              break;
+              case 'A':
+              printf("c'est une voyelle");
+              break;
+              case 'e':
+              printf("c'est une voyelle");
+	      break;
+	      case 'E':
+              printf("c'est une voyelle");
+	      break;
+              case 'i':
+              printf("c'est une voyelle");
+	      break;
+              case 'I':
+              printf("c'est une voyelle");
+	      break;
+              case 'o':
+              printf("c'est une voyelle");
+	      break;
+              case 'O':
+              printf("c'est une voyelle");
+	      break;
+	      case 'u':
+              printf("c'est une voyelle");
+	      break;
+              case 'U':
+              printf("c'est une voyelle");
+	      break;
+              case 'y':
+              printf("c'est une voyelle");
+	      break;
+	      case 'Y':
+              printf("c'est une voyelle");
+              break;
+      default:
+	      printf("c'est une consonne.");
+	  }
+      return 0;
+}
