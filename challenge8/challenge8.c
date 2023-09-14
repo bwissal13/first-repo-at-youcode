4166d05cbe817e9a67d1e169ca30bfc47af85c0c
bwissal <baazizwissal13@gmail.com>
2023-09-14 17:09:28 +0100

    challenge 8

---
+++ challenge8/challenge8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int decimal;
+   printf("Entrer le nombre en Decimal:");
+   scanf("%d",&decimal);
+   printf("le nombre en octal est:%o\n",decimal);
+   printf("le nombre en hexadecimal:%x",decimal);
+   return 0;
+
+
+}
